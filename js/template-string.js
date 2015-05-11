3d97e6bda96ef37fee5d5ec111ba75590142f41e
Ian Ramos <ianramosc@gmail.com>
2015-05-11 15:32:16 -0300

    added template-string.js

---
+++ js/template-string.js
@@ -0,0 +1,20 @@
+//multiline string with template strings
+console.log(`This is the first line.
+Now, the second line`);
+
+let nome = "Ian";
+let idade = 20;
+
+//String Interpolation
+
+//var
+console.log(`Nome: ${nome}
+Idade: ${idade}`);
+
+let peso = 80;
+let altura = 1.87;
+
+//var + expression
+console.log(`Peso: ${peso}
+Altura: ${altura}
+IMC: ${parseInt( peso / ( Math.pow(altura, 2) ) ) }`);
