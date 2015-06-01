aab13ad55cc1a85aadd14e4c4e6d9bbd129a49f4
Ian Ramos <ianramosc@gmail.com>
2015-05-31 22:59:54 -0300

    Added variable called imc and interpolation

--- js/template-string.js
+++ js/template-string.js
@@ -13,8 +13,9 @@ Idade: ${idade}`);
 
 let peso = 80;
 let altura = 1.87;
+let imc = parseInt( peso / ( Math.pow(altura, 2) ) ).toFixed(1);
 
 //var + expression
 console.log(`Peso: ${peso}
 Altura: ${altura}
-IMC: ${ parseInt( peso / ( Math.pow(altura, 2) ) ) }`);
+IMC: ${imc}`);
